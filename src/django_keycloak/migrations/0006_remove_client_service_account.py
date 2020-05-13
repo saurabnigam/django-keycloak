2799e717e9c99a1525b00cf022724552526ef824
Phoebe Bright <phoebebright310@gmail.com>
2020-05-13 13:35:35 +0100

    commenting out service_account in migrations as unable to build test database suddenly in gascloud.  Shouldn't have to do this!

--- src/django_keycloak/migrations/0006_remove_client_service_account.py
+++ src/django_keycloak/migrations/0006_remove_client_service_account.py
@@ -10,8 +10,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='client',
-            name='service_account',
-        ),
+        # migrations.RemoveField(
+        #     model_name='client',
+        #     name='service_account',
+        # ),
     ]
